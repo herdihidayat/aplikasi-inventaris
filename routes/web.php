4f631992dc8aa04c2a199d27a90374a87e2495b1
Herdin Hidayat <hidayatyaa@gmail.com>
2025-02-08 15:25:54 +0800

    banyak di edit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Admin\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('layouts.main');
-});
+// Route::get('/', function () {
+//     return view('pages.products.index');
+// });
+
+Route::get('/products', [ProductController::class, 'index']);
